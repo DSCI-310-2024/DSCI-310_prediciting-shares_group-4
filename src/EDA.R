e32df7efd711f775ccf9cfa6389da72ab0a542b6
agill59 <amargill2627@gmail.com>
2024-04-10 15:30:55 -0700

    Made changes to repository to clean it as well as changes to makefile for easier interpretation

--- src/EDA.R
+++ src/EDA.R
@@ -11,7 +11,7 @@ Options:
 # import libraries/packages
 suppressMessages(library(tidyverse))
 suppressWarnings(library(docopt))
-suppressWarnings(source("R/histograms.R"))
+suppressWarnings(library(dsci310utils))
 
 # parse/define command line arguments here
 opt <- docopt(doc)
@@ -36,7 +36,7 @@ main <- function(data, output){
     labs(title = "Boxplot of Shares") +
     ylab(label = 'Shares') +
     theme(axis.title = element_text(size = 20), axis.text = element_text(size = 15), title = element_text(size = 25))
-    ggsave(filename = paste(output1, 'shares.png', sep = ''), shares_plot)
+    ggsave(filename = paste(output1, 'shares.png', sep = ''), shares_plot, create.dir = TRUE)
 
     num_obs_training <- fullData |>
       group_by(is_popular) |>
@@ -47,17 +47,17 @@ main <- function(data, output){
     # Histogram 1: Distribution of the number of links in article
     mean_hrefs_plot <- make_histogram(fullData, "num_hrefs", "is_popular",  # nolint
     "Distribution of number of links", "Number of Links")
-    ggsave(filename = paste(output1, 'links.png', sep = ''), mean_hrefs_plot)
+    ggsave(filename = paste(output1, 'links.png', sep = ''), mean_hrefs_plot, create.dir = TRUE)
 
     # Histogram 2: Distribution of the number of images in article  
     mean_imgs_plot <- make_histogram(fullData, "num_imgs", "is_popular",  # nolint
     "Distribution of number of Images", "Number of Images")
-    ggsave(filename = paste(output1, 'images.png', sep = ''),  mean_imgs_plot)
+    ggsave(filename = paste(output1, 'images.png', sep = ''),  mean_imgs_plot, create.dir = TRUE)
 
     # Histogram 3: Distribution of the number of videos in article
     mean_videos_plot <- make_histogram(fullData, "num_videos", "is_popular",  # nolint
     "Distribution of number of Videos", "Number of Videos")
-    ggsave(filename = paste(output1, 'videos.png', sep = ''), mean_videos_plot)
+    ggsave(filename = paste(output1, 'videos.png', sep = ''), mean_videos_plot, create.dir = TRUE)
 }
 
 # code for other functions & tests goes here
