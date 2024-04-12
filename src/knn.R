4a701c6a6866440afb323952a8011aefc260fa91
Yuji <liangyanxin20@163.com>
2024-04-11 18:13:07 -0700

    added comment

--- src/knn.R
+++ src/knn.R
@@ -21,37 +21,46 @@ opt <- docopt(doc)
 # define main function
 
 main <- function(output, workflow){
+    # Convert the output directory argument to a string
     output1 <- toString(output)
+
+    # Read a serialized workflow object from a given file path
     workflow <- readRDS(toString(workflow))
-  # predicting number of shares for testing data
+
+    # Read the testing data CSV
     articles_testing <- suppressMessages(read_csv('data/testing_data.csv'))
     articles_testing <- articles_testing |> mutate(is_popular = as.factor(is_popular))
 
+    # predicting number of shares for testing data
     articles_predict <- predict(workflow, articles_testing) |>
     bind_cols(articles_testing)
 
+    # Calculate the accuracy of the predictions, filtering the results to only include accuracy metric
     articles_accuracy <- articles_predict |>
     metrics(truth = is_popular, estimate = .pred_class) |>
     filter(.metric == "accuracy")
+
+    # Save the accuracy results to a CSV file 
     write_csv(articles_accuracy, paste('data/', 'model_accuracy.csv', sep = ''))
 
+    # Generate a confusion matrix for the predictions
     confusion_matrix <- articles_predict |>
     conf_mat(truth = is_popular, estimate = .pred_class)
 
+    # Save the confusion matrix object as an RDS file in the specified output directory
     saveRDS(confusion_matrix, paste(output1, 'objects/conf_mat.rds', sep = ''))
 
+    # Extract true positive, false positive, false negative, and true negative counts from the confusion matrix
     TP <- confusion_matrix$table[2, 2]  
     FP <- confusion_matrix$table[2, 1]  
     FN <- confusion_matrix$table[1, 2]
     TN <- confusion_matrix$table[1, 1]  
 
+    summarize the confusion matrix and save the summary to the 'data/' directory
     create_conf_mat_summary(TP,FP,FN,TN,'data/')
 
 }
 
-# code for other functions & tests goes here
-
-
 
 # call main function
-main(opt$output, opt$workflow) # pass any command line args to main here
+main(opt$output, opt$workflow) 
